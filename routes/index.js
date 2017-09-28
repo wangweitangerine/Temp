8bf6fc3990049ffc343202847b5505c30b962b02
wangweitangerine <wangwei@tangerinetech.com>
2017-09-28 15:59:28 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var transliteration=require("transliteration");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+
+console.info(transliteration.transliterate("你好"));
 
 module.exports = router;
